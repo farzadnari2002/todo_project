8194fd8b7218e9aeceb782ce3765aea055268229
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-09-20 12:19:28 +0330

    category model and serializer created

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.title
+
+
 class Todo(models.Model):
+    Category = models.ManyToManyField(Category,  related_name='todos')
     title = models.CharField(max_length=100)
     task = models.TextField()
     priority = models.IntegerField()
--- todo/serializers.py
+++ todo/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Todo
+from .models import *
 
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+    
 
 class TodoSerializer(ModelSerializer):
+    
+    
     class Meta:
         model = Todo
         fields = '__all__'
+        
+        
+
     
