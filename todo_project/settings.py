8fbda5ebdcdbc8a7c2c5d2ef603b9bdf6a89341a
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-10-13 23:56:05 +0330

    use default pagination and persmission for test

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -126,5 +126,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 2,
+    'DEFAULT AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.BasicAuthentication',
+        ],
+    'DEFAILT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+    
 }
