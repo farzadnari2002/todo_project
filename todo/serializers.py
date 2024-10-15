7a4a1781592cb6d1cf4d19b1732c60dc81c415ae
Farzad Nari <157991524+farzadnari2002@users.noreply.github.com>
2024-10-15 17:10:39 +0330

    define validate_priority for TodoSerializer test

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from .models import *
+from rest_framework import serializers
 
         
 class TodoSerializer(ModelSerializer):
+    
+    def validate_priority(self, priority):
+        if priority == 0:
+            raise serializers.ValidationError('not can 0')
+        return priority
     
     class Meta:
         model = Todo
